d1b0adb27d5d503520f3a39190d8301e29b5e182
sasaSilver <al.mikhailov@innopolis.university>
2024-10-05 04:31:12 +0300

    ver. 1 needs testing

--- simplex_method.py
+++ simplex_method.py
@@ -1,20 +1,21 @@
 import numpy as np
+import sys
 
 simplex_input = (
-    [-6, -10, -4],          # C - objective func. coefficients
+    [6, 9],          # C - objective func. coefficients
     [
-        [1, 1, 1],
-        [1, 1, 0],
-        [1, 2, 0],
+        [2, 3],
+        [1, 1],
     ],               # A - constraint coefficients
-    [1000, 500, 700],         # b - rhs of constraints
+    [12, 5],         # b - rhs of constraints
     1e-4,            # e - precision
-    "min"            # max or min
+    "max"            # max or min
 )
 
 np.set_printoptions(suppress=True)
+np.seterr(divide='ignore')
 
-def simplex(input_data: tuple = simplex_input) -> list[int] | str:
+def simplex(input_data: tuple = simplex_input) -> list | str:
 
     # parsing input data
     c_objective: np.ndarray = np.array(input_data[0], dtype=float)
@@ -37,7 +38,7 @@ def simplex(input_data: tuple = simplex_input) -> list[int] | str:
         return "Unbounded solution!"
 
     # printing the optimization problem
-    str_problem: str = ("max" if max else "min") + f" z = {c_objective[0]} * x1"
+    str_problem: str = f"problem: {"max" if max else "min"} z = {c_objective[0]} * x1"
     for i, coefficient in enumerate(c_objective[1:]):
         if coefficient == 0:
             term = ""
@@ -46,7 +47,7 @@ def simplex(input_data: tuple = simplex_input) -> list[int] | str:
         else:
             term = f" + {coefficient} * x{i + 2}"
         str_problem += term
-    str_problem = str_problem[:-3] + "\nsubject to the constraints:\n"
+    str_problem += "\nsubject to the constraints:\n"
     for i, coefficients in enumerate(constraints):
         for j, coefficient in enumerate(coefficients):
             str_problem += f"{coefficient} * x{j + 1} + "
@@ -61,11 +62,9 @@ def simplex(input_data: tuple = simplex_input) -> list[int] | str:
     table[1:, :-(1 + n_constraints)] = constraints
     # fill in rhs
     table[1:, -1] = rhs
-
     
     # simplex loop
     while np.any(table[0, :-1] < -precision):
-        print(table)
         # index of the minimal element in z-row
         pivot_col: int = np.argmin(table[0, :-1])
         
@@ -77,7 +76,7 @@ def simplex(input_data: tuple = simplex_input) -> list[int] | str:
             return "Unbounded problem!"
         
         # ignore negative elements (for the next line)
-        ratios[ratios <= 0] = np.inf
+        ratios[abs(ratios) < precision] = np.inf
         
         # ratios are built starting from the second row, so add one
         pivot_row: int = np.argmin(ratios) + 1
@@ -87,11 +86,35 @@ def simplex(input_data: tuple = simplex_input) -> list[int] | str:
         # normalize pivot row
         table[pivot_row] /= pivot_elem
         
+        # do this idk
         for i in range(n_constraints + 1):
             if i != pivot_row:
                 table[i] -= table[i, pivot_col] * table[pivot_row]
-    return table
+    
+    solution = []
+    
+    # collect solution
+    for i in range(n_vars):
+        # find a basic variable
+        if (1 - precision < np.sum(table[1:, i])  <  1 + precision) and (np.max(table[1:, i]) == 1):
+            basic_row: int = np.where(table[:, i] == 1)[0][0]
+            basic_value: float = table[basic_row, -1]
+            # place basic variable's index and its value
+            solution.append((i + 1, float(basic_value)))
+    # if finidng min, multiply by -1
+    if not max:
+        table[0] *= -1
+    # place the z value in the end
+    solution.append(float(table[0, -1]))
+    return solution
     
     
-    
-print(simplex())
+res = simplex()
+
+if isinstance(res, str):
+    print(res, file=sys.stderr)
+else:
+    output_str: str = f"solution: z = {res.pop()}, where\n"
+    for i, value in res:
+        output_str += f"x{i} = {value}, "
+    print(output_str)
