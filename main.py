9d773dc0f5f5c0bbd5d91a60b8b8b6fe716dbf5d
sasaSilver <al.mikhailov@innopolis.university>
2024-10-06 02:01:14 +0300

    v123123

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from simplex_method import simplex, print_simplex_result
 
 lpp = {
-    "C": [10, 4],          # C - objective function coefficients list
+    "C": [10, 6.5],       # C - objective function coefficients
     "A": [                # A - constraint coefficients matrix
-        [20, 10],
+        [20.3, 10],
         [40, 10],
     ],                    
-    "b": [1200, 1600],         # b - rhs of constraints list
-    "e": 1e-4,            # e - precision float
+    "b": [1200, 1600],    # b - rhs of constraints
+    "e": 1e-4,            # e - precision
     "max": True           # max or min - True or False
 }
 
--- simplex_method.py
+++ simplex_method.py
@@ -2,9 +2,10 @@ import numpy as np
 import sys
 
 np.set_printoptions(suppress=True)
+# ignore division by zero errors, as we need to get infinities
 np.seterr(divide='ignore')
 
-def simplex(lpp: dict) -> list | str:
+def simplex(lpp: dict) -> tuple | str:
     # parsing input data
     c_objective: np.ndarray = np.array(lpp["C"], dtype=float)
     constraints: np.ndarray = np.array(lpp["A"], dtype=float)
@@ -30,47 +31,47 @@ def simplex(lpp: dict) -> list | str:
     # simplex loop while there are negative entries in the z-row
     while np.any(table[0, :-1] < -precision):
         # index of the minimal element in z-row
-        pivot_col: int = np.argmin(table[0, :-1])
+        pivot_column: int = np.argmin(table[0, :-1])
         
         # find ratios (rhs/pivot_column_element)
-        ratios: np.ndarray = table[1:, -1] / table[1:, pivot_col]
+        ratios: np.ndarray = table[1:, -1] / table[1:, pivot_column]
         
         # problem is unbounded if all elements are < 0 or infinite
-        if np.all((ratios <= precision) | np.isinf(ratios)):
+        if np.all((ratios < -precision) | np.isinf(ratios)):
             return "Unbounded problem!"
         
-        # ignore negative elements (for the next line)
-        ratios[abs(ratios) < precision] = np.inf
+        # ignore negative elements and zeros in the ratios
+        ratios[ratios < -precision] = np.inf
+        ratios[-precision < ratios < precision] = np.inf
         
         # ratios are built starting from the second row, so add one
         pivot_row: int = np.argmin(ratios) + 1
 
-        pivot_elem: float = table[pivot_row, pivot_col]
+        pivot_element: float = table[pivot_row, pivot_column]
         
         # normalize pivot row
-        table[pivot_row] /= pivot_elem
+        table[pivot_row] /= pivot_element
         
         # do this idk
         for i in range(n_constraints + 1):
             if i != pivot_row:
-                table[i] -= table[i, pivot_col] * table[pivot_row]
+                table[i] -= table[i, pivot_column] * table[pivot_row]
     
-    solution = []
+    solution_indexes_values = []
     
     # collect solution
     for i in range(n_vars):
         # find a basic variable
         if (1 - precision < np.sum(table[1:, i])  <  1 + precision) and (np.max(table[1:, i]) == 1):
+            # basic row is where the '1' is located
             basic_row: int = np.where(table[:, i] == 1)[0][0]
             basic_value: float = table[basic_row, -1]
             # place basic variable's index and its value
-            solution.append((i + 1, float(basic_value)))
+            solution_indexes_values.append((i + 1, float(basic_value)))
     # if finidng min, multiply by -1
-    if not max:
-        table[0] *= -1
-    # place the z value in the end
-    solution.append(float(table[0, -1]))
-    return solution
+    z_value = float(table[0, -1]) * (1 if min else -1)
+    
+    return solution_indexes_values, z_value
 
 def check_lpp(lpp: dict) -> None:
     c_objective, constraints, rhs, precision, __ = lpp.values()
@@ -86,7 +87,6 @@ def check_lpp(lpp: dict) -> None:
 
 def print_lpp(lpp: dict) -> None:
     c_objective, constraints, rhs, _, max = lpp.values()
-    print(c_objective, constraints, rhs, _, max)
     str_problem: str = f"problem:\n{"max" if max else "min"} z = {c_objective[0]:g} * x1"
     for i, coefficient in enumerate(c_objective[1:]):
         if coefficient == 0:
@@ -104,16 +104,16 @@ def print_lpp(lpp: dict) -> None:
     print(str_problem[:-1])
 
 
-def print_simplex_result(res: str | list) -> None:
+def print_simplex_result(res: str | tuple) -> None:
     if isinstance(res, str):
         print(res, file=sys.stderr)
-    else:
-        output_str: str = f"solution:\nz = {res.pop():g}"
-        if len(res) == 0:
-            print(output_str)
-            return
-        output_str += ',\n'
-        for i, value in res:
-            output_str += f"x{i} = {value:g},\n"
-        output_str = output_str[:-2]
+        return
+    output_str: str = f"solution:\nz = {res[-1]:g}"
+    if len(res) == 1:
         print(output_str)
+        return
+    output_str += ',\n'
+    for i, value in res[0]:
+        output_str += f"x{i} = {value:g},\n"
+    output_str = output_str[:-2]
+    print(output_str)
